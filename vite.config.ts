99dfe7567db55306ff5c41e193b1510c14b2918f
vo1x <ak2061@icloud.com>
2025-08-11 23:21:08 +0545

    update: api key import via .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vitest/config";
+import dotenv from "dotenv";
 
+dotenv.config();
 export default defineConfig({
   test: {
     environment: "node",
+    globals: true,
     setupFiles: ["./tests/setup.ts"],
-    include: ["tests/**/*.test.ts"]
-  }
+    include: ["tests/**/*.test.ts"],
+  },
 });
