24a0559e1c67b4dcbeebcec625ca91ad45d71135
vo1x <ak2061@icloud.com>
2025-08-12 19:28:03 +0545

    ✨feat: basic tv series endpoints now supported

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export * from "./types";
 export * from "./movie";
+export * from './tv'
--- src/services/types.ts
+++ src/services/types.ts
@@ -41,3 +41,84 @@ export interface ImagesResponse {
   backdrops: Array<{ file_path: string; width: number; height: number }>;
   posters: Array<{ file_path: string; width: number; height: number }>;
 }
+
+export interface ITVSeries {
+  id: number;
+  name: string;
+  original_name: string;
+  overview: string;
+  first_air_date?: string;
+  last_air_date?: string;
+  status?: string;
+  tagline?: string;
+  type?: string;
+  homepage?: string;
+  in_production?: boolean;
+  languages?: string[];
+  number_of_episodes?: number;
+  number_of_seasons?: number;
+  origin_country?: string[];
+  original_language?: string;
+  popularity?: number;
+  vote_average?: number;
+  vote_count?: number;
+  poster_path?: string | null;
+  backdrop_path?: string | null;
+  genres?: { id: number; name: string }[];
+  created_by?: {
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path: string | null;
+  }[];
+  episode_run_time?: number[];
+  last_episode_to_air?: EpisodeSummary;
+  next_episode_to_air?: EpisodeSummary;
+  networks?: {
+    id: number;
+    name: string;
+    logo_path: string | null;
+    origin_country: string;
+  }[];
+  production_companies?: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries?: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  seasons?: {
+    air_date?: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string | null;
+    season_number: number;
+    vote_average: number;
+  }[];
+  spoken_languages?: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+}
+
+export interface EpisodeSummary {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string | null;
+}
